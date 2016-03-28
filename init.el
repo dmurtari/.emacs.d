0a8fc4ff06fb76234e15d7d443a49ae2c94a6ed3
Domenic Murtari <domenic.murtari@gmail.com>
2016-03-28 11:08:55 -0600

    Changed theme to Solarized dark

--- init.el
+++ init.el
@@ -9,19 +9,12 @@
 (ac-config-default)
 
 (setq-default indent-tabs-mode nil)
+(setq tab-width 2)
 (setq inhibit-startup-message t)
+(setq backup-inhibited t)
+(setq auto-save-default nil)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
+(load-theme 'solarized t)
+(set-frame-parameter nil 'background-mode 'dark)
+(set-terminal-parameter nil 'background-mode 'dark)
