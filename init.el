29b41dbd39f1d99b305a212409bef489d8a80e5a
Domenic Murtari <domenic.murtari@gmail.com>
2016-03-29 14:41:12 -0600

    Revert back to Auto Complete

--- init.el
+++ init.el
@@ -5,37 +5,61 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
-;; Company Mode Config
-(add-hook 'after-init-hook 'global-company-mode)
+;; Requires
+(require 'auto-complete)
+(require 'auto-complete-config)
+(require 'ac-html)
+(require 'web-mode)
+
+;; AutoComplete Mode Config
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+(global-auto-complete-mode)
+(add-to-list 'ac-modes 'web-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
-(setq company-idle-delay 0)
 
 ;; Web Mode Config
-(require 'web-mode)
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
 (setq web-mode-engines-alist
       '(("angular" . "\\.html\\'")
-        ("scss"    . "\\.css\\'"))
+        ("scss"    . "\\.scss\\'"))
       )
 (setq web-mode-enable-auto-pairing t)
 (setq web-mode-enable-css-colorization t)
 (setq web-mode-enable-current-element-highlight t)
+(add-hook 'html-mode-hook 'setup-ac-for-html)
+(add-to-list 'web-mode-ac-sources-alist
+             '("html" . (ac-source-html-tag
+                         ac-source-html-attr
+                         ac-source-html-attrv)))
+(add-hook 'web-mode 'ac-html-angular+)
 
 ;; Mode Settings
 (global-git-gutter-mode t)
 (indent-guide-global-mode)
 (setq indent-guide-recursive t)
 (projectile-global-mode)
+(setq js2-basic-offset 2)
+(setq js2-bounce-indent-p t)
+(setq js2-cleanup-whitespace t)
+(setq js2-enter-indents-newline t)
+(setq js2-highlight-level 3)
+(setq js2-indent-on-enter-key t)
+(custom-set-variables '(coffee-tab-width 2))
 
 ;; File Type Configurations
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . angular-html-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
 
 ;; Defaults
 (show-paren-mode 1)
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t)
 (electric-pair-mode 1)
 (electric-indent-mode 1)
 (setq-default column-number-mode t)
@@ -57,3 +81,16 @@
 ;; Keybindings
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x f") 'projectile-find-file)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+
+;; Function
+(defun setup-ac-for-html ()
+  (require 'ac-html)
+  (require 'ac-html-default-data-provider)
+  (ac-html-enable-data-provider 'ac-html-default-data-provider)
+  (ac-html-setup)
+  (setq ac-sources '(ac-source-html-tag
+                     ac-source-html-attr
+                     ac-source-html-attrv))
+  (auto-complete-mode))
+
