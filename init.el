aaee3f667b0fd45b4f6c5bdba9d5f36170055046
Domenic Murtari <domenic.murtari@gmail.com>
2016-03-31 15:01:46 -0600

    Cleanup

--- init.el
+++ init.el
@@ -108,13 +108,9 @@
   (auto-complete-mode))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+
  )
 
 (defun auto-complete-mode-maybe ()
-  "No maybe for you. Only AC!"
   (unless (minibufferp (current-buffer))
     (auto-complete-mode 1)))
